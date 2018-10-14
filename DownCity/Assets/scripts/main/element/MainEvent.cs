ff92aac44a32f0405ca23dda50f819a64730d865
wushengzhong <wushengzhong@gmail.com>
2018-10-14 18:28:58 +0800

    feat: 添加了点击事件

---
+++ DownCity/Assets/scripts/main/element/MainEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MainEvent : MonoBehaviour {
+
+    public Image img;
+
+	public void EventFunction () {
+        switch(gameObject.name) {
+            case "tree": Debug.Log("===>点击了 树"); break;
+            case "market": Debug.Log("===>点击了 市场"); break;
+            case "blacksmith": Debug.Log("===>点击了 铁匠铺"); break;
+            case "manor": Debug.Log("===>点击了 庄园"); break;
+            case "pub": Debug.Log("===>点击了 酒馆"); break;
+            case "warehouse": Debug.Log("===>点击了 仓库"); break;
+            case "cemetery": Debug.Log("===>点击了 墓地"); break;
+            case "battle": Debug.Log("===>点击了 火堆"); break;
+            case "castle": Debug.Log("===>点击了 城堡"); break;
+        }
+    }
+
+}
